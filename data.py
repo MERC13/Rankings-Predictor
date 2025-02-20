6a60462bb9f7252f460955020d7e61a3eeb68302
Jonah Cha <jonahc426@gmail.com>
2025-02-20 15:45:26 -0800

    gitignore and adding mhq

--- data.py
+++ data.py
@@ -80,11 +80,170 @@ def tournaments():
                 [40, 13]
             ],
             [
-                
+                [32, 22],   # Ebert - Stanton vs. Miller - Wang
+                [43, 9],    # R. Mikes - H. Schiffmayer vs. Congdon - Magness
+                [15, 10],   # Hayes - Quinones vs. Cazares - Ormand
+                [23, 12],   # Brown - Kendrick vs. Pfeifer - Pfeifer
+                [46, 59],   # Ludwig - Troyer vs. E. Jeub - B. Miller
+                [11, 52],   # Calhoun - Hall vs. Houk - Neal
+                [29, 7],    # Beck - Businelle vs. Geller - Spurling
+                [50, 51],   # Hernandez - Hernandez vs. Laliberty - Schreffler
+                [13, 49],   # Hetzel - Jacobs vs. Beck - Stetler
+                [55, 19],   # Hager - Ludwig vs. McNamara - Werner
+                [20, 47],   # Aucott - Furr vs. Ciembronowicz - Hager
+                [27, 2],    # McKinnon - McKinnon vs. Miller - Reeder
+                [34, 30],   # Haas - Strobel vs. Moore - Moyles
+                [28, 58],   # Jeub - Sommers vs. Businelle - Reiger
+                [38, 4],    # King - King vs. Moore - Williams
+                [16, 48],   # T. Mikes - J. Schiffmayer vs. Kurtz - Lycan
+                [6, 3],     # Vitvitska - Wu vs. Eltiste - Eltiste
+                [36, 42],   # Stage - Stage vs. Bates - Eschen
+                [39, 62],   # Hayes - Lathrop vs. Stetler - Trowbridge
+                [18, 54],   # Flynn - Strohmeyer vs. Fuentes - Nwankwo
+                [5, 56],    # Carli - Smith vs. Clinefelter - Clinefelter
+                [53, 61],   # Eschen - Riddle vs. Hayes - Visser
+                [25, 37],   # Choi - Parsons vs. Forcina - Vicars
+                [57, 45],   # Benson - Vigil vs. Blackwell - Troyer
+                [24, 26],   # Nicholas - Shipley vs. Z. Jeub - D. Miller
+                [1, 21],    # Kausrud - Reeder vs. Julian - Mann
+                [17, 8],    # Ruiz - Ruiz vs. Aucott - Stage
+                [35, 41],   # Lin - Wu vs. McCarthy - Pfeifer
+                [33, 60],   # Johnson - Johnson III vs. Bates - Barrios
+                [14, 31],   # Jacobs - Taylor vs. Gallegos - Robbins
+                [44, 40]    # Reiger - Strobel vs. Benton - Knight
             ],
             [
-                
+                [52, 20],  # Houk - Neal vs. Aucott - Furr
+                [35, 46],  # Lin - Wu vs. Ludwig - Troyer
+                [45, 39],  # Blackwell - Troyer vs. Hayes - Lathrop
+                [27, 54],  # McKinnon - McKinnon vs. Fuentes - Nwankwo
+                [44, 15],  # Reiger - Strobel vs. Hayes - Quinones
+                [55, 61],  # Hager - Ludwig vs. Hayes - Visser
+                [50, 17],  # Hernandez - Hernandez vs. Ruiz - Ruiz
+                [33, 13],  # Johnson - Johnson III vs. Hetzel - Jacobs
+                [62, 32],  # Stetler - Trowbridge vs. Ebert - Stanton
+                [34, 28],  # Haas - Strobel vs. Jeub - Sommers
+                [40, 30],  # Benton - Knight vs. Moore - Moyles
+                [51, 9],   # Laliberty - Schreffler vs. Congdon - Magness
+                [11, 5],   # Calhoun - Hall vs. Carli - Smith
+                [47, 43],  # Ciembronowicz - Hager vs. R. Mikes - H. Schiffmayer
+                [10, 56],  # Cazares - Ormand vs. Aucott - Stage
+                [3, 6],    # Eltiste - Eltiste vs. McNamara - Werner
+                [58, 49],  # Businelle - Reiger vs. Clinefelter - Clinefelter
+                [37, 42],  # Forcina - Vicars vs. Bates - Eschen
+                [41, 59],  # McCarthy - Pfeifer vs. Vitvitska - Wu
+                [23, 7],   # Brown - Kendrick vs. Beck - Stetler
+                [60, 22],  # Bates - Barrios vs. Miller - Wang
+                [31, 59],  # Gallegos - Robbins vs. E. Jeub - B. Miller
+                [4, 7],    # Moore - Williams vs. Geller - Spurling
+                [18, 24],  # Flynn - Strohmeyer vs. Nicholas - Shipley
+                [29, 26],  # Beck - Businelle vs. Z. Jeub - D. Miller
+                [57, 1],   # Benson - Vigil vs. Kausrud - Reeder
+                [38, 14],  # King - King vs. Jacobs - Taylor
+                [53, 25],  # Eschen - Riddle vs. Choi - Parsons
+                [12, 2],   # Pfeifer - Pfeifer vs. Miller - Reeder
+                [36, 16],  # Stage - Stage vs. T. Mikes - J. Schiffmayer
+                [48, 21]   # Kurtz - Lycan vs. Julian - Mann
             ],
+            [
+                [25, 3],   # Choi - Parsons vs. Eltiste - Eltiste
+                [30, 4],   # Moore - Moyles vs. Moore - Williams
+                [19, 44],  # McNamara - Werner vs. Reiger - Strobel
+                [8, 34],   # Aucott - Stage vs. Haas - Strobel
+                [61, 41],  # Hayes - Visser vs. McCarthy - Pfeifer
+                [6, 12],   # Vitvitska - Wu vs. Pfeifer - Pfeifer
+                [7, 31],   # Geller - Spurling vs. Gallegos - Robbins
+                [28, 47],  # Jeub - Sommers vs. Ciembronowicz - Hager
+                [14, 18],  # Jacobs - Taylor vs. Flynn - Strohmeyer
+                [39, 38],  # Hayes - Lathrop vs. King - King
+                [26, 40],  # Z. Jeub - D. Miller vs. Benton - Knight
+                [16, 55],  # T. Mikes - J. Schiffmayer vs. Hager - Ludwig
+                [49, 60],  # Beck - Stetler vs. Bates - Barrios
+                [56, 48],  # Clinefelter - Clinefelter vs. Kurtz - Lycan
+                [21, 45],  # Julian - Mann vs. Blackwell - Troyer
+                [54, 62],  # Fuentes - Nwankwo vs. Stetler - Trowbridge
+                [32, 51],  # Ebert - Stanton vs. Laliberty - Schreffler
+                [43, 50],  # R. Mikes - H. Schiffmayer vs. Hernandez - Hernandez
+                [17, 29],  # Ruiz - Ruiz vs. Beck - Businelle
+                [1, 58],   # Kausrud - Reeder vs. Businelle - Reiger
+                [15, 53],  # Hayes - Quinones vs. Eschen - Riddle
+                [42, 52],  # Bates - Eschen vs. Houk - Neal
+                [24, 57],  # Nicholas - Shipley vs. Benson - Vigil
+                [2, 11],   # Miller - Reeder vs. Calhoun - Hall
+                [13, 36],  # Hetzel - Jacobs vs. Stage - Stage
+                [46, 27],  # Ludwig - Troyer vs. McKinnon - McKinnon
+                [5, 33],   # Carli - Smith vs. Johnson - Johnson III
+                [9, 10],   # Congdon - Magness vs. Cazares - Ormand
+                [22, 35],  # Miller - Wang vs. Lin - Wu
+                [20, 23],  # Aucott - Furr vs. Brown - Kendrick
+                [59, 37]   # E. Jeub - B. Miller vs. Forcina - Vicars
+            ],
+            [
+                [9, 11],   # Congdon - Magness vs. Calhoun - Hall
+                [33, 18],  # Johnson - Johnson III vs. Flynn - Strohmeyer
+                [43, 44],  # R. Mikes - H. Schiffmayer vs. Reiger - Strobel
+                [22, 20],  # Miller - Wang vs. Aucott - Furr
+                [49, 54],  # Beck - Stetler vs. Fuentes - Nwankwo
+                [3, 15],   # Eltiste - Eltiste vs. Hayes - Quinones
+                [45, 28],  # Blackwell - Troyer vs. Jeub - Sommers
+                [24, 58],  # Nicholas - Shipley vs. Businelle - Reiger
+                [6, 17],   # Vitvitska - Wu vs. Ruiz - Ruiz
+                [34, 37],  # Haas - Strobel vs. Forcina - Vicars
+                [50, 55],  # Hernandez - Hernandez vs. Hager - Ludwig
+                [42, 57],  # Bates - Eschen vs. Benson - Vigil
+                [47, 41],  # Ciembronowicz - Hager vs. McCarthy - Pfeifer
+                [40, 29],  # Benton - Knight vs. Beck - Businelle
+                [5, 39],   # Carli - Smith vs. Hayes - Lathrop
+                [61, 32],  # Hayes - Visser vs. Ebert - Stanton
+                [48, 12],  # Kurtz - Lycan vs. Pfeifer - Pfeifer
+                [38, 53],  # King - King vs. Eschen - Riddle
+                [23, 60],  # Brown - Kendrick vs. Bates - Barrios
+                [59, 56],  # E. Jeub - B. Miller vs. Clinefelter - Clinefelter
+                [4, 13],   # Moore - Williams vs. Hetzel - Jacobs
+                [19, 8],   # McNamara - Werner vs. Aucott - Stage
+                [14, 21],  # Jacobs - Taylor vs. Julian - Mann
+                [27, 35],  # McKinnon - McKinnon vs. Lin - Wu
+                [51, 31],  # Laliberty - Schreffler vs. Gallegos - Robbins
+                [10, 16],  # Cazares - Ormand vs. T. Mikes - J. Schiffmayer
+                [52, 62],  # Houk - Neal vs. Stetler - Trowbridge
+                [7, 2],    # Geller - Spurling vs. Miller - Reeder
+                [25, 30],  # Choi - Parsons vs. Moore - Moyles
+                [46, 26],  # Ludwig - Troyer vs. Z. Jeub - D. Miller
+                [1, 36]    # Kausrud - Reeder vs. Stage - Stage
+            ],
+            [
+                [55, 59],  # Hager - Ludwig vs. E. Jeub - B. Miller
+                [13, 19],  # Hetzel - Jacobs vs. McNamara - Werner
+                [26, 51],  # Z. Jeub - D. Miller vs. Laliberty - Schreffler
+                [53, 23],  # Eschen - Riddle vs. Brown - Kendrick
+                [20, 4],   # Aucott - Furr vs. Moore - Williams
+                [21, 22],  # Julian - Mann vs. Miller - Wang
+                [12, 24],  # Pfeifer - Pfeifer vs. Nicholas - Shipley
+                [17, 25],  # Ruiz - Ruiz vs. Choi - Parsons
+                [57, 34],  # Benson - Vigil vs. Haas - Strobel
+                [56, 42],  # Clinefelter - Clinefelter vs. Bates - Eschen
+                [58, 46],  # Businelle - Reiger vs. Ludwig - Troyer
+                [18, 1],   # Flynn - Strohmeyer vs. Kausrud - Reeder
+                [8, 27],   # Aucott - Stage vs. McKinnon - McKinnon
+                [62, 47],  # Stetler - Trowbridge vs. Ciembronowicz - Hager
+                [28, 7],   # Jeub - Sommers vs. Geller - Spurling
+                [39, 43],  # Hayes - Lathrop vs. R. Mikes - H. Schiffmayer
+                [54, 33],  # Fuentes - Nwankwo vs. Johnson - Johnson III
+                [37, 14],  # Forcina - Vicars vs. Jacobs - Taylor
+                [16, 9],   # T. Mikes - J. Schiffmayer vs. Congdon - Magness
+                [60, 38],  # Bates - Barrios vs. King - King
+                [36, 10],  # Stage - Stage vs. Cazares - Ormand
+                [30, 52],  # Moore - Moyles vs. Houk - Neal
+                [44, 61],  # Reiger - Strobel vs. Hayes - Visser
+                [11, 3],   # Calhoun - Hall vs. Eltiste - Eltiste
+                [29, 49],  # Beck - Businelle vs. Beck - Stetler
+                [2, 40],   # Miller - Reeder vs. Benton - Knight
+                [32, 50],  # Ebert - Stanton vs. Hernandez - Hernandez
+                [15, 5],   # Hayes - Quinones vs. Carli - Smith
+                [31, 6],   # Gallegos - Robbins vs. Vitvitska - Wu
+                [35, 48],  # Lin - Wu vs. Kurtz - Lycan
+                [41, 45]   # McCarthy - Pfeifer vs. Blackwell - Troyer
+            ]
         ]
     ]
     
